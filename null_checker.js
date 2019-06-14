ecd6c73af2e05f0d557aa4dcc2601d46b86213c5
babirye470 <51475326+babirye470@users.noreply.github.com>
2019-06-14 08:30:03 -0400

    added file null_checker via upload

---
+++ null_checker.js
@@ -0,0 +1,12 @@
+var i;
+for(i=1; i<=10;i++)
+{
+	let number = prompt('please input a number?','');
+	if(number>0)
+		{console.log(1);}
+	else if(number>0)y
+		{console.log(-1);}
+	else
+		{console.log(0);}
+
+}
